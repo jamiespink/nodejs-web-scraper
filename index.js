312baf1d29fbaae289d3db50827ca633fd32994b
Jamie Spink <jamie.spink@qmmedia.se>
2021-04-10 14:10:41 +0200

    scraping page headers

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-import { getHTML, getTwitterFollowers } from './lib/scraper';
+import { getHTML, getHeader } from './lib/scraper';
 
 
 async function go() {
-    getTwitterFollowers(await getHTML('http://books.toscrape.com/'));
+    const homePromise = getHTML('http://books.toscrape.com/index.html');
+    const booksPromise = getHTML('http://books.toscrape.com/catalogue/category/books_1/index.html');
+    const [homeHTML, booksHTML] = await Promise.all([homePromise, booksPromise]);
+    const homeHeader = await getHeader(homeHTML);
+    const booksHeader = await getHeader(booksHTML);
+    console.log(`Home header: '${homeHeader}', Books header: '${booksHeader}'`);
 }
 
 go();
