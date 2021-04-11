5dcd0ef3084ff53e6fe2c27e6ff90e0cd2f5adbe
Jamie Spink <jamiespink2@gmail.com>
2021-04-11 21:07:19 +0200

    refactored functions

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-import { getHTML, getHeader } from './lib/scraper';
+import { getHomeHeader, getBooksHeader } from './lib/scraper';
 
 
 async function go() {
-    const homePromise = getHTML('http://books.toscrape.com/index.html');
-    const booksPromise = getHTML('http://books.toscrape.com/catalogue/category/books_1/index.html');
-    const [homeHTML, booksHTML] = await Promise.all([homePromise, booksPromise]);
-    const homeHeader = await getHeader(homeHTML);
-    const booksHeader = await getHeader(booksHTML);
-    console.log(`Home header: '${homeHeader}', Books header: '${booksHeader}'`);
+    const [home, books] = await Promise.all([getHomeHeader(), getBooksHeader()]);
+    console.log(`Home: ${home}, Books: ${books}`);
+//     const homePromise = getHTML('http://books.toscrape.com/index.html');
+//     const booksPromise = getHTML('http://books.toscrape.com/catalogue/category/books_1/index.html');
+//     const [homeHTML, booksHTML] = await Promise.all([homePromise, booksPromise]);
+//     const homeHeader = getHeader(homeHTML);
+//     const booksHeader = getHeader(booksHTML);
+//     console.log(`Home header: '${homeHeader}', Books header: '${booksHeader}'`);
 }
 
 go();
