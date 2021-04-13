e98dab72d8f012cc48e61ec45761f877a27c652d
Jamie Spink <jamie.spink@wis-ltd.net>
2021-04-13 17:51:59 +0200

    cron job working

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-import express from 'express';
-import { getHomeHeader, getBooksHeader } from './lib/scraper';
-import db from './lib/db';
+import express from "express";
+import "./lib/cron";
 
 const app = express();
 
-console.log(db);
-
-app.get('/scrape', async (req, res, next) => {
-    console.log('scraping...');
-    const [home, books] = await Promise.all([getHomeHeader(), getBooksHeader()]);
-    console.log(`Home: ${home}, Books: ${books}`);
-    res.json({home, books});
+app.get("/scrape", async (req, res, next) => {
+  
 });
 
-app.listen(3000, (details) => console.log('App running on port 3000'));
+app.listen(3000, () => console.log("App running on port 3000"));
