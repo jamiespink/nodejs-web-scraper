031b52f2212650fe638cfe383b886c749774b28e
Jamie Spink <jamiespink2@gmail.com>
2021-04-14 21:18:20 +0200

    api endpoint connected to db

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import db from "./lib/db";
 import "./lib/cron";
 
 const app = express();
 
-app.get("/scrape", async (req, res, next) => {
-  
+app.get("/data", async (req, res, next) => {
+    const home = db.value();
+    res.json(home);
 });
 
-app.listen(3000, () => console.log("App running on port 3000"));
+app.listen(3000, () => console.log("App running on http://localhost:3000"));
