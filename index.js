32d7cf9ae595e0019fc3a9ead784feb557f398b2
Jamie Spink <jamiespink2@gmail.com>
2021-04-15 21:25:09 +0200

    got puppeteer working

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import db from "./lib/db";
+import { startBrowser } from "./lib/browser";
+import { pageController } from "./lib/pageController";
 import "./lib/cron";
+
+let browserInstance = startBrowser();
+pageController(browserInstance);
 
 const app = express();
 
